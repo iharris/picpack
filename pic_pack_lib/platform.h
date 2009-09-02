6d3c1758f9d4d120fd7e5e25fb8da8c8dd5fceb5
Ian Harris <imharris@gmail.com>
2009-09-02 19:19:41 +0100

    Corrected comment on tech toys board

--- pic_pack_lib/platform.h
+++ pic_pack_lib/platform.h
@@ -13,7 +13,7 @@
 #define OLIMEX_BOARD  2
 /** SparkFun RF Terminal Development Node */
 #define SFE_TDN_V1    3
-/** Tech Toys HK Pic 17f4550 Eval Dev 4a */
+/** Tech Toys HK Pic 18f4550 Eval Dev 4a */
 #define TECH_TOYS_PIC18F4550 4
 /** Olimex PIC LCD with Nokia 3310 LCD dispaly */
 #define OLIMEX_PIC_LCD3310 5
